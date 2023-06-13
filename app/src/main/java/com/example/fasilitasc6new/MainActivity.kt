857c1e876008015beed805ee1f08e746bc377f7f
ariandto <92358210+ariandto@users.noreply.github.com>
2023-06-14 05:03:26 +0700

    Initial commit

---
+++ app/src/main/java/com/example/fasilitasc6new/MainActivity.kt
@@ -0,0 +1,10 @@
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.fasilitasc6new.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.tampilan_1)
+    }
+}
